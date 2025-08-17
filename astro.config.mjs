c9cc4f7dcb14f9cd0fbe6edde1447ff18885b897
Raimundo Coelho <raimundocf.ti@gmail.com>
2025-08-17 18:43:24 -0300

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://divcomp.com.br",
+  integrations: [mdx(), sitemap()],
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
